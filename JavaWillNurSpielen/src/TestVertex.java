c39ed9008b70da8aefbe821805faf83b42b9d531
59enrico <enileue@gmail.com>
2017-10-10 15:19:27 +0200

    added skalarMult; skalarMultMod; add; addMod

--- JavaWillNurSpielen/src/TestVertex.java
+++ JavaWillNurSpielen/src/TestVertex.java
@@ -4,8 +4,13 @@ public class TestVertex {
 	public static void main(String[] args) {
 		
 		Vertex v1 = new Vertex (2,2);	//definiere irgendein Punkt für Vertex unter Vertex v1
+		Vertex v2 = v1.skalarMult(3.5);	//neues Vertex aus kopie von v1*double s Wert in Klammern hier: 3.5
+		
 		System.out.println(v1);			//Ausgabe des Punktes als String
+		System.out.println(v2);			//Ausgabe von v2
+	
 		System.out.println(v1.lenght());	//Ausgabe des Abstandes des Punktes vom Ursprung
+		
 	}
 
 }
--- JavaWillNurSpielen/src/Vertex.java
+++ JavaWillNurSpielen/src/Vertex.java
@@ -4,17 +4,37 @@ public class Vertex {
 
 	public double x;
 	public double y;
+	//public double s;
 	
-	public Vertex(double x, double y){
+	public Vertex(double x, double y) {
 		this.x = x;		//2 x Werte; this.x als das x vom Objekt; x als fremder Wert
 		this.y = y;		//2 y Werte; this.y als das y vom Objekt; y als fremder Wert
+		//this.s = s;
 	}
 	
-	public String toString(){
+	public String toString() {
 		return "("+x+","+y+")";	//Ausgabe als nicht String, sondern als Wert
 	}
 	
-	public double lenght(){
+	public double lenght() {
 		return Math.sqrt(x*x+y*y);	//Wurzel aus x^2 & y^2
+	}
+
+	public Vertex skalarMult(double s) {
+		return new Vertex(x*s,y*s);
+	}
+	
+	public void skalarMultMod(double s) {
+		x=x*s;
+		y=y*s;
+	}
+	
+	public Vertex add(Vertex v) {
+		return new Vertex(v.x+x, v.y+y);
+	}
+	
+	public void addMod(Vertex v) {
+		x=v.x+x;
+		y=v.y+y;
 	}
 }
