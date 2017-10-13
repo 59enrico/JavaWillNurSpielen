f5c970000ab5cf3e3e471e79ebe9e134af636e84
Leue.Enrico <Leue.Enrico@SK-D43D7EB3B8BB.2631.skool.net>
2017-10-13 09:00:59 +0200

    New Method's; addad GeometricObjects

--- JavaWillNurSpielen/src/TestVertex.java
+++ JavaWillNurSpielen/src/TestVertex.java
@@ -3,20 +3,25 @@ public class TestVertex {
 
 	public static void main(String[] args) {
 		
-		Vertex v1 = new Vertex (2,2);	//definiere irgendein Punkt für Vertex unter Vertex v1
-		Vertex v2 = v1.skalarMult(3.5);	//neues Vertex aus kopie von v1*double s Wert in Klammern hier: 3.5
+		Vertex v1 = new Vertex (4,76);	//definiere irgendein Punkt für Vertex unter Vertex v1
+	//	Vertex v2 = v1.skalarMult(3.5);	//neues Vertex aus kopie von v1*double s Wert in Klammern hier: 3.5
 		
-		Vertex v3 = v1.add(v2);			//addiere v2 auf v1 als v3
+	//	Vertex v3 = v1.add(v2);			//addiere v2 auf v1 als v3
 		//oder
 		//v1.addMod(v2);					//als Befehl v1 addiere v2 auf dich drauf, v1+v2 wird später unter v1 ausgegeben nicht wie in Z.9 als v3
 		
 		
 		System.out.println(v1);			//Ausgabe des Punktes als String
-		System.out.println(v2);			//Ausgabe von v2
-		System.out.println(v3);			//Ausgabe von v3
+	//	System.out.println(v2);			//Ausgabe von v2
+	//	System.out.println(v3);			//Ausgabe von v3
 	
-		System.out.println(v1.lenght());	//Ausgabe des Abstandes des Punktes vom Ursprung
+	//	System.out.println(v1.length());	//Ausgabe des Abstandes des Punktes vom Ursprung
 		
+		v1.normalize();
+		System.out.println(v1);
+		System.out.println(v1.getX()+v1.getY());
+		
+	
 	}
 
 }
--- JavaWillNurSpielen/src/Vertex.java
+++ JavaWillNurSpielen/src/Vertex.java
@@ -9,14 +9,13 @@ public class Vertex {
 	public Vertex(double x, double y) {
 		this.x = x;		//2 x Werte; this.x als das x vom Objekt; x als fremder Wert
 		this.y = y;		//2 y Werte; this.y als das y vom Objekt; y als fremder Wert
-		//this.s = s;
 	}
 	
 	public String toString() {
-		return "("+x+","+y+")";	//Ausgabe als nicht String, sondern als Wert
+		return "("+x+","+y+")";	//Ausgabe nicht als String, sondern als Wert
 	}
 	
-	public double lenght() {
+	public double length() {
 		return Math.sqrt(x*x+y*y);	//Wurzel aus x^2 & y^2
 	}
 
@@ -38,16 +37,42 @@ public class Vertex {
 		y=v.y+y;
 	}
 	
-	public void setX(double x) {
-		this.x = x;
-	}
+	public void setX(double x) {this.x = x;}
 	
-	public void setY(double y) {
-		this.y = y;
-	}
+	public void setY(double y) {this.y = y;}
 
 	public double getX() {return x;} 
 	
 	public double getY() {return y;}
 	
+	public boolean equals(Object thatObject) {
+		if (thatObject instanceof Vertex) {
+			Vertex that = (Vertex)thatObject;
+			
+			return this.x==that.x && this.y==that.y;
+		}
+		return false;
+	}
+
+	public Vertex sub(Vertex that) {
+		return new Vertex(x-that.x, y-that.y);
+	}
+	
+	public void subMod(Vertex that) {
+		x=x-that.x;
+		y=y-that.y;
+	}
+	
+	public double distance(Vertex v) {
+		double xx = this.x-v.x;
+		double yy = this.y-v.y;
+		
+		return Math.sqrt(xx*xx+yy*yy);
+	}
+	
+	public void normalize() {
+		double factor = length();
+		x=x/factor;
+		y=y/factor;
+	}
 }
