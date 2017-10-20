90963f67e102e2021e9ab5aa11f375ea5254563c
Leue.Enrico <Leue.Enrico@SK-D43D7EB3B8BB.2631.skool.net>
2017-10-20 08:22:39 +0200

    Overload Constructor GeometricObject

--- JavaWillNurSpielen/src/GeometricObject.java
+++ JavaWillNurSpielen/src/GeometricObject.java
@@ -1,4 +1,41 @@
 
 public class GeometricObject {
 
+	public Vertex pos;
+	public double width;
+	public double height;
+	
+	public double getWidth() {return width;}
+	public double getHeight() {return height;}
+	public Vertex getPos() {return pos;}	
+	
+public GeometricObject(Vertex pos, double width, double height){
+	this.pos=pos;
+	this.width=width;
+	this.height=height;
+	  
+	if(width<0){
+		width=-width;
+		pos.x=pos.x-width;
+	}
+  
+  
+	if(height<0){
+		height=-height;
+		pos.y=pos.y-height;
+	}
+}
+
+public GeometricObject(double width, double height){
+	this(new Vertex(0,0), width, height);
+}
+
+public GeometricObject(double width){
+	this(width, width);
+}
+
+public GeometricObject(){this(10);}
+
+public GeometricObject(Vertex pos){this(pos,0,0);}
+
 }
